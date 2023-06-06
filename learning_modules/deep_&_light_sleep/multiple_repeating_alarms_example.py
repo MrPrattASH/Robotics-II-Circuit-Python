ef272a06016fd95b0af7fcd124b1dfd63432fc22
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2023-06-06 10:44:49 +0200

    Update multiple_repeating_alarms_example.py

--- learning_modules/deep_&_light_sleep/multiple_repeating_alarms_example.py
+++ learning_modules/deep_&_light_sleep/multiple_repeating_alarms_example.py
@@ -1,7 +1,6 @@
-'''multiple pin or timer alarms example
-- The first section initializes the button as an input with a pulldown resistor.
-- The second section initializes the onboard LED as an output.
-- The third section sets up a list of alarms that will trigger when the button is pressed or after 10 seconds of deep sleep.
+'''MULTIPLE ALARMS example
+- The first section initializes the onboard LED as an output.
+- The second section sets up a list of alarms that will trigger when the button is pressed or after 10 seconds of deep sleep.
 - The while loop turns off the onboard LED when sleep starts and puts the board into deep sleep mode until either alarm is triggered.
 - When either alarm is triggered, it turns on the onboard LED and repeats the deep sleep program with the same alarms set.
 '''
@@ -29,10 +28,12 @@ led.value = False
 #check if a time based alarm woke us up
 if isinstance(alarm.wake_alarm, alarm.time.TimeAlarm):
     print("\nA TIME alarm woke us up!")
+    #do other things here we want to do if time has elapsed
 
 #check if a pin alarm woke us up (no way to know WHAT pin, just that A pin woke us)
 elif isinstance(alarm.wake_alarm, alarm.pin.PinAlarm):
     print("\nA PIN alarm woke us up!")
+    #do other things here we want to do if a button was pressed. 
 
 alarm.exit_and_deep_sleep_until_alarms(pin_alarm1, time_alarm)
 #we never get here as we now exit and deep sleep
